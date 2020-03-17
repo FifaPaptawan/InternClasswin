1a4587e7a01e83ca74864d65e82e0f6d03c8dc98
Paptawan Panyapanichkul <paptawan.tcs@gmail.com>
2020-03-17 17:30:08 +0700

    Add action

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Task from "./Task";
 
-function Tasklist({tasks, loading }) {
+function Tasklist({tasks, loading, actions}) {
+    
     if (loading) {
         return <div className= "List-items">loading</div>;
     }
-if (tasks.lenght == 0) {
-    return <div className="List-items">no-items</div>
 
-  
+    if (tasks.lenght === 0) {
+        return <div className="List-items">no-items</div>
     }
-    return(
-    <div className="List-items">
-        {tasks.map(task => (
-            <Task task={task} /> 
-        ))}
-    </div>
+
+    return (
+        <div className="List-items">
+            {tasks.map(task => (
+                <Task task={task} {...actions} /> 
+            ))}
+        </div>
     );
 }
 
