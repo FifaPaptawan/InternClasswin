55dd317843bb325274190fce613acc4f53ccdb25
Paptawan Panyapanichkul <paptawan.tcs@gmail.com>
2020-03-17 18:07:51 +0700

    Add LoadingRow in Loading screen

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,9 +2,29 @@ import React from "react";
 import Task from "./Task";
 
 function Tasklist({tasks, loading, actions}) {
+
+    const  LoadingRow = (
+        <div className="loading-item">
+            <span className="glow-checkbox" />
+            <span className="glow-text" >
+                <span>Loading</span>
+                <span>cool</span>
+                <span>state</span>
+             </span>
+        </div>
+    );
     
     if (loading) {
-        return <div className= "List-items">loading</div>;
+        return (
+            <div className= "List-items">
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+            </div>
+        );
     }
 
     if (tasks.lenght === 0) {
